3d6c1b58b438a3ba277bf9081dc27f2b6de43e95
ergouwang <mrergouwang@gmail.com>
2020-03-09 19:14:40 +0800

    base.less common.less

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer id="footer">
+    <p class="page-tail">@Multi user blog management platform</p>
+  </footer>
+</template>
+
+<style lang="less" scoped>
+  @import "../assets/base.less";
+  footer{
+    background-color:@color-footer-text;
+
+    p{
+      text-align: center;
+      padding: 10px 0;
+    }
+
+  }
+</style>
